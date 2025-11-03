5f551d109b0003ade4a313c383246ef8c4561a6f
Erik <elindstr@student.chalmers.se>
2025-11-03 16:35:28 +0100

    Enhance Warning/Error + Other Output Messages

--- scripts/add_solution.py
+++ scripts/add_solution.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 from generate_readme import generate_readme, SUPPORTED_LANGUAGES
 
 
-def dir_number_exists(number: int):
+def numbered_dir_exists(number: int):
     """
     Returns boolean for whether problem folder with the given number exists.
     """
@@ -43,17 +43,18 @@ parser = argparse.ArgumentParser(
 # Add command line arguments
 parser.add_argument("number", type=int, help="leetcode problem number")
 parser.add_argument("url", help="URL link to leetcode problem")
-parser.add_argument( "--languages", "--l", nargs="*", help="language source files to add - case insensitive")
+parser.add_argument( "--language", "--l", nargs="*", help="programming language names of source files to add (case insensitive)")
 
 # Parse arguments
 args = parser.parse_args()
 number: int = args.number
 url:str = args.url.strip()
-languages: list[str] = [lang.strip().lower() for lang in args.languages or []]
+languages: list[str] = [lang.strip().lower() for lang in args.language or []]
 
 # Check for number mismatch; problem folder already existing
-if dir_number_exists(number):
-    print(f"Error! Number mismatch/typo. Problem folder with number '{number:04d}' already exists.")
+if numbered_dir_exists(number):
+    print("WARNING: Number mismatch/typo!")
+    print(f"> Problem folder with the number '{number:04d}' already exists.")
     exit(1)
 
 # Pre-compute supported language names before loop
@@ -63,7 +64,7 @@ supported_lang_names = {name for name, _, _ in SUPPORTED_LANGUAGES}
 unsupported = [lang for lang in languages if lang not in {name.lower() for name in supported_lang_names}]
 if unsupported:
     quoted_output = [f"'{lang}'" for lang in unsupported]
-    print(f"Error! Unsupported languages: {', '.join(quoted_output)}")
+    print(f"Unsupported language(s): {', '.join(quoted_output)}")
     print(f"Please try one of: {', '.join(supported_lang_names)}")
     exit(1)
 
@@ -107,7 +108,7 @@ try:
     with open(folder_path / "README.md", "x", encoding="utf-8") as f:
         f.write(PROBLEM_README_TEMPLATE.format(number, title, url))
 except FileExistsError:
-    print(f"README already exists at {folder_path}, skipping...")
+    print(f"Warning: README already exists at {folder_path}, skipping...")
 
 # Create new program files with the appropriate extension
 # Throws an error if they already exist
@@ -123,7 +124,7 @@ for lang in languages:
                 with open(folder_path / (source_file_name + lang_extension), "x", encoding="utf-8") as f:
                     f.write(FILE_HEADER.format(lang_comment, number, title, url))
             except FileExistsError:
-                print(f"'{lang_name}' source file already exists at {folder_path}, skipping...")
+                print(f"Warning: '{lang_name}' source file already exists at {folder_path}, skipping...")
 
 
 # Regenerate main README file
