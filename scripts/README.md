9f1f973558d744e14aeb4c04fcddb760f090819b
Erik <elindstr@student.chalmers.se>
2025-11-18 23:46:27 +0100

    Update Main README Template + Add README in '/scripts'

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+# Scripts
+
+- `add_solution-py`: automatically adds directories + files and calls `generate_readme()`. Flag "--help" for script usage.
+- `generate_readme.py`: generates new **main** README.md file with updated *Contents* table.
+- `config.py`: exports filepaths and supported languages dict.
+- `templates.py`: exports string templates for READMEs and source file header.
--- scripts/templates.py
+++ scripts/templates.py
@@ -5,12 +5,6 @@ Collection of my solutions to various leetcode problems.
 
 <!--TODO (maybe):  add statistics section with # of problems solved, which difficulty, etc.-->
 
-## Scripts
-- `add_solution-py`: automatically adds directories + files and calls `generate_readme()`. Use flag "--help" for script usage.
-- `generate_readme.py`: generates new main README.md file with updated *Contents* table.
-- `config.py`: exports filepaths and supported languages dict.
-- `templates.py`: exports string templates for READMEs and source file header.
-
 ## Contents
 | # | Problem Title | Language(s) | Link |
 |:-:|---------------|-------------|------|"""
